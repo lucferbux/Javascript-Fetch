8d76a96846b32bb9d08fbfbe5fd465fff16c91d4
Lucas Fernandez <lucasfernandezaragon@gmail.com>
2021-02-19 12:27:42 +0100

    ⚙️ WIP, trying to get id

--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import './styles/main.scss'
 const btn = document.getElementById("btn-download");
 
 btn.addEventListener("click", () => {
-    const videoUrl = 'https://run.mocky.io/v3/2393a2af-b036-4bab-babd-b01691c78568';
+    const videoUrl = 'https://firebasestorage.googleapis.com/v0/b/lucferbux-web-page.appspot.com/o/la_caja.mp4?alt=media&token=6c90cca9-a1d3-45ab-8205-16982ccc8c7c';
+    const checkUrlImage = '2ocykBzWDiM';
 
-    requestVideo(videoUrl, () => {
-
-        console.log("is working");
-
+    requestVideo(checkUrlImage, () => {
         var videoPlayer = document.getElementById("videoPlayer");
-
         videoPlayer.style.display = "block";
+        videoPlayer.src = videoUrl;
 
     });
 });
--- src/utils.js
+++ src/utils.js
@@ -15,16 +15,36 @@ function poll(fn, retries = Infinity, timeoutBetweenAttempts = 1000){
 }
 
 
+
+var imageCheck = (id) => {
+    return new Promise(function (resolve, rejected) {
+
+        var img = new Image();
+		img.src = "http://img.youtube.com/vi/" + id + "/mqdefault.jpg";
+		img.onload = function () {
+			resolve(this.width);
+		}
+    });
+  };
+
 export const  requestVideo = (url, callback) => {
-    function validate(res){
+    function validateUrl(res){
         if(!res || res.status !== 200)  {
             throw res; 
         } else {
             callback();
-        }
-            
+        }        
     }
-    return poll(() => axios.get(url).then(validate), 15, 1000);
+
+    function validatePreview(width) {
+        if (width === 120) {
+			throw "error"; 
+		} else {
+            callback();
+        }
+    }
+    //return poll(() => axios.get(url, {headers: {'Access-Control-Allow-Origin': '*'}}).then(validateUrl), 15, 1000);
+    return poll(() => imageCheck(url).then(validatePreview), 15, 1000);
 }
 
   
